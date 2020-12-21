bdb5085d171bd6e6e2e6863bc2b1b6dec0b9a585
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-21 13:03:41 -0700

    shift removes first item in array, pop removes last item in array

---
+++ not_project_files/javascript-arrays/1 - Store Multiple Values in an Array/4 - Remove Elements From an Array/start/js/remove.js
@@ -0,0 +1,14 @@
+const shoppingList = [
+    'bread',
+    'butter',
+    'honey',
+    'coffee'
+];
+
+//shift removes the first item in an array
+shoppingList.shift();
+
+// pop removes the last item in an array
+shoppingList.pop();
+
+console.log(shoppingList);
