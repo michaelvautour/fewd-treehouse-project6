8d7454343a15588c0a3609878ff44b2bce21a028
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-21 12:51:57 -0700

    Using push to add elements to end of array and unshift to add elements to start of an array

--- not_project_files/javascript-arrays/1 - Store Multiple Values in an Array/3 - Add Elements to an Array/start/js/add.js
+++ not_project_files/javascript-arrays/1 - Store Multiple Values in an Array/3 - Add Elements to an Array/start/js/add.js
@@ -1,1 +1,10 @@
 const instruments = ['piano', 'drums', 'trumpet'];
+
+//push will add elements to end of an array
+instruments.push('guitar', 'violin', 'triangle');
+
+//unshift will add elements to beginning of an array
+instruments.unshift('saxophone', 'tuba')
+
+console.log(instruments);
+
