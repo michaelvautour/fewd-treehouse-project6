58c73cd5d2d6daf2e27d850ef8be9738cc478e33
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-21 16:35:25 -0700

    spreading in arrays

--- not_project_files/javascript-arrays/1 - Store Multiple Values in an Array/5 - Copy and Combine Arrays with the Spread Operator/start/js/spread.js
+++ not_project_files/javascript-arrays/1 - Store Multiple Values in an Array/5 - Copy and Combine Arrays with the Spread Operator/start/js/spread.js
@@ -1,2 +1,7 @@
 const middle = ['lettuce', 'cheese', 'patty'];
 const burger = ['top bun', 'bottom bun'];
+
+const burgers = [...middle, ...burger];
+console.log(burgers);
+// console.log(burger);
+// burger.length
