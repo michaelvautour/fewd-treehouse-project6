6af2783f95830e48f899efa2b27f952764ca482e
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-23 11:55:59 -0700

    Implemented case sensitivity checking, as well as capitalizing first letter only and remainder lower characters on output

--- not_project_files/javascript-arrays/2 - Loop Through Arrays/4 - Search for a Value in an Array/start/js/search-list.js
+++ not_project_files/javascript-arrays/2 - Loop Through Arrays/4 - Search for a Value in an Array/start/js/search-list.js
@@ -1,3 +1,5 @@
+
+
 const inStock = [
     'pizza',
     'cookies',
@@ -18,12 +20,26 @@ const inStock = [
 const search = prompt('Search for a product.');
 let message;
 
+// This will set the array to lower case to allow for a lower case to
+// lower case comparison during the if else
 for (let i=0; i < inStock.length; i++) {
     inStock[i] = inStock[i].toLowerCase();
   }
 
-if (inStock.includes(search.toLowerCase())) {
-    console.log(`Your item ${search} is in stock!`)
-} else {
-    console.log(`Your item ${search} is not in stock!"`);
+// This will make is to the searched array string will output as first 
+// letter capital and remainder lower case
+function capitalFirst(string) {
+    for (i = 0; i < string.length; i++) {
+        string1 = string.toLowerCase();
+    }
+    return string.charAt(0).toUpperCase() + string1.slice(1);
 }
+
+// This will compare the users search criteria against the array for a match
+if (inStock.includes(search.toLowerCase())) {
+    message = `Your item <strong>${capitalFirst(search)}</strong> is in stock!`
+} else {
+    message = `Your item <strong>${capitalFirst(search)}</strong> is not in stock!"`
+}
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`;
