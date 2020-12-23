4f9112f5af99608e449123f10683aca09bf08855
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-23 10:39:02 -0700

    search and setting lowercase to compare values

--- not_project_files/javascript-arrays/2 - Loop Through Arrays/4 - Search for a Value in an Array/start/js/search-list.js
+++ not_project_files/javascript-arrays/2 - Loop Through Arrays/4 - Search for a Value in an Array/start/js/search-list.js
@@ -1,3 +1,29 @@
-const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+const inStock = [
+    'pizza',
+    'cookies',
+    'eggs', 
+    'apples', 
+    'milk', 
+    'cheese', 
+    'bread', 
+    'lettuce', 
+    'carrots', 
+    'broccoli', 
+    'potatoes', 
+    'crackers', 
+    'onions', 
+    'tofu', 
+    'limes', 
+    'cucumbers'];
 const search = prompt('Search for a product.');
 let message;
+
+for (let i=0; i < inStock.length; i++) {
+    inStock[i] = inStock[i].toLowerCase();
+  }
+
+if (inStock.includes(search.toLowerCase())) {
+    console.log(`Your item ${search} is in stock!`)
+} else {
+    console.log(`Your item ${search} is not in stock!"`);
+}
