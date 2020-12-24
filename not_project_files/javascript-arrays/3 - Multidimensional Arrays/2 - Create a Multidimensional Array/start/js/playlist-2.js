9ab657ac31efd0630acaece11814d611c5a0dc68
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-23 19:08:58 -0700

    working quiz

--- not_project_files/javascript-arrays/3 - Multidimensional Arrays/2 - Create a Multidimensional Array/start/js/playlist-2.js
+++ not_project_files/javascript-arrays/3 - Multidimensional Arrays/2 - Create a Multidimensional Array/start/js/playlist-2.js
@@ -1,11 +1,14 @@
 const playlist = [
-  'So What',
-  'Respect',
-  'What a Wonderful World',
-  'At Last',
-  'Three Little Birds',
-  'The Way You Look Tonight'
+  ['So What', 'Miles Davis', '9:04'],
+  ['Respect', 'Aretha Franklin', '2:45'],
+  ['What a Wonderful World', 'Louis Armstrong', '2:21'],
+  ['At Last', 'Ella Fitzgerald', '4:18'],
+  ['Three Little Birds', 'Bob Marley and the Wailers', '3:01'],
+  ['The Way You Look Tonight', 'Frank Sinatra', '3:21']
 ];
+
+const myArtists = `${playlist[0][1]}, ${playlist[1][1]}`;
+console.log(myArtists);
 
 function createListItems( arr ) {
   let items = '';
--- not_project_files/javascript-arrays/3 - Multidimensional Arrays/3 - Build a Quiz Challenge/start/js/quiz.js
+++ not_project_files/javascript-arrays/3 - Multidimensional Arrays/3 - Build a Quiz Challenge/start/js/quiz.js
@@ -1,9 +1,14 @@
 // 1. Create a multidimensional array to hold quiz questions and answers
 
+const questions = [
+  ['What planet do we live on?', 'earth'],
+  ['What is the name of the energy element that heats the planet', 'sun'],
+  ['What body orbits the earth?', 'moon']
+];
 
 // 2. Store the number of questions answered correctly
 
-
+let correctAnswers = 0;
 /* 
   3. Use a loop to cycle through each question
       - Present each question to the user
@@ -12,5 +17,22 @@
         answered questions increments by 1
 */
 
+for ( let i = 0; i < questions.length; i++ ) {
+  let question = questions[i][0];
+  let answer = questions[i][1];
+  let response = prompt(question);
+
+  if (response === answer) {
+    correctAnswers++;
+  }
+}
+
 
 // 4. Display the number of correct answers to the user
+
+let html = `
+<h1>You got ${correctAnswers} question(s) correct </h1>
+<p></p>
+`
+
+document.querySelector('main').innerHTML = html;
