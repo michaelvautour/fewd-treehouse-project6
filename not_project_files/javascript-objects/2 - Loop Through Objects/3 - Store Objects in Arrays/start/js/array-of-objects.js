5a69daee8bbdb6c4f0e512b3f552adf215d2782d
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-26 14:27:55 -0700

    converted quiz array object to object literals in an array. Simplified the approach to pulling in data

--- not_project_files/javascript-objects/2 - Loop Through Objects/3 - Store Objects in Arrays/start/js/array-of-objects.js
+++ not_project_files/javascript-objects/2 - Loop Through Objects/3 - Store Objects in Arrays/start/js/array-of-objects.js
@@ -1,9 +1,17 @@
 // 1. Create an array to hold quiz questions and answers
 const questions = [
-  ['How many planets are in the Solar System?', '8'],
-  ['How many continents are there?', '7'],
-  ['How many legs does an insect have?', '6'],
-  ['What year was JavaScript created?', '1995']
+  {question: 'How many planets are in the Solar System?',
+   answer: '8'
+  },
+  {question: 'How many continents are there?',
+  answer: '7'
+},
+  {question: 'How many legs does an insect have?',
+  answer: '6'
+},
+  {question: 'What year was JavaScript created?',
+  answer: '1995'
+}
 ];
 
 // 2. Store the number of questions answered correctly
@@ -19,8 +27,8 @@ let correctAnswers = 0;
         answered questions increments by 1
 */
 for ( let i = 0; i < questions.length; i++ ) {
-  let question = questions[i][0];
-  let answer = questions[i][1];
+  let question = questions[i].question;
+  let answer = questions[i].answer;
   let response = prompt(question);
   
   if ( response === answer ) {
