22c0542a9d258173c04b67a770dc9e3a5796eca5
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-26 13:54:38 -0700

    used method join within template literal to call all array values in object literal into a string

--- not_project_files/javascript-objects/1 - Object Basics/2 - Create an Object Literal/start/js/object.js
+++ not_project_files/javascript-objects/1 - Object Basics/2 - Create an Object Literal/start/js/object.js
@@ -1,13 +1,17 @@
 
 // below is an object literal ( it's between {} vs. [] for arrays)
 const person = {
-    name: "Mike",
+    name: "Michael",
     city: 'Cold Lake',
     age: 37,
     isStudent: true,
     skills: ['JavaScript', 'HTML', 'CSS']
 }
 
-const message = `Hi, I'm ${person.name}. I am learning ${person.skills[0]}, ${person.skills[1]} and ${person.skills[2]}! I started this journey when I was ${person.age} from the city of ${person.city}. It is ${person.isStudent} I am a student!`
+person.nickname = 'Mike';
+// using dot notation to access the object literals, combined with template literals
+const message = `Hi, I'm ${person.name}, you can call me ${person.nickname}. I am learning ${person.skills[0]}, ${person.skills[1]} and ${person.skills[2]}! I started this journey when I was ${person.age} in the city of ${person.city}. It is ${person.isStudent} I am a student!`
+const message1 = `Hi, I'm ${person.name}, you can call me ${person.nickname}. I am learning ${person.skills.join(', ')}! I started this journey when I was ${person.age}, just a few months shy from ${person.age + 1} in the city of ${person.city}. It is ${person.isStudent} I am a student!`
 
 console.log(message);
+console.log(message1);
