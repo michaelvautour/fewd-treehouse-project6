b3ccaefb36d67fb566078105c46d046d5f22397d
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-26 13:19:35 -0700

    created first object literal

---
+++ not_project_files/javascript-objects/1 - Object Basics/2 - Create an Object Literal/start/js/object.js
@@ -0,0 +1,10 @@
+
+// below is an object literal ( it's between {} vs. [] for arrays)
+const person = {
+    name: "Mike",
+    city: 'Calgary',
+    age: 37,
+    isStudent: true,
+    skills: ['JavaScript', 'HTML', 'CSS']
+}
+
