e9195f6b4f7e5210c131bdb0778c21484c3fea15
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-26 13:27:29 -0700

    using object literals to create a message using template literals

--- not_project_files/javascript-objects/1 - Object Basics/2 - Create an Object Literal/start/js/object.js
+++ not_project_files/javascript-objects/1 - Object Basics/2 - Create an Object Literal/start/js/object.js
@@ -2,9 +2,12 @@
 // below is an object literal ( it's between {} vs. [] for arrays)
 const person = {
     name: "Mike",
-    city: 'Calgary',
+    city: 'Cold Lake',
     age: 37,
     isStudent: true,
     skills: ['JavaScript', 'HTML', 'CSS']
 }
 
+const message = `Hi, I'm ${person.name}. I am learning ${person.skills[0]}, ${person.skills[1]} and ${person.skills[2]}! I started this journey when I was ${person.age} from the city of ${person.city}. It is ${person.isStudent} I am a student!`
+
+console.log(message);
