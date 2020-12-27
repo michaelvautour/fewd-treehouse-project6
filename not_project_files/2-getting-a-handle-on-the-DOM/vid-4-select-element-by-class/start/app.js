e4e78e786cca1b634aa40ea4c29bb953eb196416
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-27 11:22:55 -0700

    practice files with tag name dom manipulation through loops

--- not_project_files/2-getting-a-handle-on-the-DOM/vid-4-select-element-by-class/start/app.js
+++ not_project_files/2-getting-a-handle-on-the-DOM/vid-4-select-element-by-class/start/app.js
@@ -1,4 +1,4 @@
-const myList = document.getElementsByTagName('li');
+const myList = document.getElementsByClassName('helloWorld');
 
 for (let i = 0; i < myList.length; i += 1) {
   myList[i].style.color = 'purple';
