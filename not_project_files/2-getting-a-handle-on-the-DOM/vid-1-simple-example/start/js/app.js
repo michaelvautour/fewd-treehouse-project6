7e3bd585e4e77a2cba34f1bc5e61bc6d0b01c764
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-27 11:01:22 -0700

    manipulated dom through getElementById, GetElementByTagName which this one requires the specific index unless you do a for loop. Also utilized button by passing values to update colors

--- not_project_files/2-getting-a-handle-on-the-DOM/vid-1-simple-example/start/js/app.js
+++ not_project_files/2-getting-a-handle-on-the-DOM/vid-1-simple-example/start/js/app.js
@@ -1,7 +1,19 @@
 const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
 
-myHeading.addEventListener('click', () => {
-    myHeading.style.color =  `rgb(${rgbColor()}, ${rgbColor()}, ${rgbColor()})`;
+
+// This is to change color using button
+
+// myButton.addEventListener('click', () => {
+//     myHeading.style.color =  `rgb(${rgbColor()}, ${rgbColor()}, ${rgbColor()})`;
+//     // myHeading.insertAdjacentHTML("beforebegin", '<p>How did you do that?</p>');
+// });
+
+// This is to change color text
+
+myButton.addEventListener('click', () => {
+    myHeading.style.color = myTextInput.value;
     // myHeading.insertAdjacentHTML("beforebegin", '<p>How did you do that?</p>');
 });
 
