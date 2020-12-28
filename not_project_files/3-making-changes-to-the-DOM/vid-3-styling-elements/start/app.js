d527b905bc2d14b88aabe170d4b31a930572796a
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-28 13:41:37 -0700

    show hide buttons either using display properties or 'innerText'. Additionally updating field elements using textContent vs. .innerHTML

--- not_project_files/3-making-changes-to-the-DOM/vid-3-styling-elements/start/app.js
+++ not_project_files/3-making-changes-to-the-DOM/vid-3-styling-elements/start/app.js
@@ -1,8 +1,34 @@
-const input = document.querySelector('input');
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+
+const input = document.querySelector('input.description');
 const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const button = document.querySelector('button.description');
+
+//My Solution
+toggleList.addEventListener('click', () => {
+  if (toggleList.innerText == 'Show List') {
+    toggleList.textContent = 'Hide List';
+    listDiv.style.display = 'block';
+  }
+  else {
+    toggleList.textContent = 'Show List';
+    listDiv.style.display = 'none';
+  }
+});
+
+///////COURSE IMPLEMENTED IT AS FOLLOWS:
+// toggleList.addEventListener('click', () => {
+//   if (listDiv.style.display == 'none') {
+//     toggleList.textContent = 'Hide List';
+//     listDiv.style.display = 'block';
+//   }
+//   else {
+//     toggleList.textContent = 'Show List';
+//     listDiv.style.display = 'none';
+//   }
+// });
 
 button.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
 });
-
