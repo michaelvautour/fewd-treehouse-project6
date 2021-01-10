c0f4a0e1641d87d71b57803e24a4123f53ced8c3
Michael Vautour <michael.e.vautour@gmail.com>
2021-01-10 14:23:52 -0700

    game now initializes on querySelector, added game phrases now need to access those randomly

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
 console.log('app.js in project 6 test')
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+
 const gameStart = document.querySelector('.btn__reset');
 
-gameStart.addEventListener('click', (e) => {
-    console.log("hello World");
-    document.getElementById('.overlay').style.display = 'none';
+const phrases = [
+"Tomorrow is going to be amazing",
+"What shall we ever do",
+"Battlestar Galactica is a great show",
+"Whatever works for you",
+"Hello World is simple but effective"
+];
 
+const gameLength = phrases.length;
+
+missedLetter = 0;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+// listen for the start game button to be pressed
+gameStart.addEventListener('click', (e) => {
+    document.getElementById('overlay').style.display = 'none';
+    getRandomPhraseAsArray;
 });
+
+//returns a random phrase from an array for the game
+const getRandomPhraseAsArray = arr => {
+    const randomPhrase = getRandomInt(gameLength);
+    return randomPhrase;
+}
+
+// adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+// check if a letter is in the phrase
+const checkLetter = button => {
+
+}
+
+// check if the game has been won or lost 
+const checkWin = () => {
+
+}
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', () => {
+
+})
