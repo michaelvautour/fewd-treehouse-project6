0bd1af81a5e4a1d1606ae1efaf25ff50376840f1
Michael Vautour <michael.e.vautour@gmail.com>
2021-01-16 16:10:56 -0700

    addphrases complete, working on checkList functions / as well as keyboard pressed

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ console.log('app.js in project 6 test')
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const ul = document.querySelector('ul')
-
 const gameStart = document.querySelector('.btn__reset');
 
 const phrases = [
@@ -20,31 +19,32 @@ function getRandomInt(max) {
   }
 
 // listen for the start game button to be pressed, update so main screen disappears
+// and new overlay exist with phrase pulled in
 gameStart.addEventListener('click', (e) => {
     document.getElementById('overlay').style.display = 'none';
-    addPhraseToDisplay()
+    addPhraseToDisplay();
 });
 
 //returns a random phrase from an array for the game
 const getRandomPhraseAsArray = arr => {
     const gameLength = phrases.length;
-    indexOfPhrase = getRandomInt(gameLength)
+    indexOfPhrase = getRandomInt(gameLength);
     console.log(indexOfPhrase);
-    let charSplit = phrases[indexOfPhrase]
-    let splitPhrase = charSplit.split('')
+    let charSplit = phrases[indexOfPhrase];
+    let splitPhrase = charSplit.split('');
     return splitPhrase;
 }
 
 function addPhraseToDisplay() {
     const chars = getRandomPhraseAsArray()
     for (i = 0; i < chars.length; i++) {
-        let char = chars[i]
-        let createLi = document.createElement('li')
+        let char = chars[i];
+        let createLi = document.createElement('li');
         createLi.textContent = char
             if (char === ' ') {
-                createLi.classList.add('space')
+                createLi.classList.add('space');
             } else {
-                createLi.classList.add('letter')
+                createLi.classList.add('letter');
             }
         ul.appendChild(createLi)
         }
@@ -52,9 +52,10 @@ function addPhraseToDisplay() {
 
 
 // check if a letter is in the phrase
-const checkLetter = button => {
-
+function checkLetter() {
+    console.log("whatever");
 }
+
 
 // check if the game has been won or lost 
 const checkWin = () => {
@@ -62,6 +63,11 @@ const checkWin = () => {
 }
 
 // listen for the onscreen keyboard to be clicked
-qwerty.addEventListener('click', () => {
-
-});
+qwerty.addEventListener('click', (e) => {
+    let match = null;
+    let button = get('button')
+    if (e.target.tagName == 'button');
+            if (button.textContent = "q") {
+            console.log("Hello World");
+            }
+    });
